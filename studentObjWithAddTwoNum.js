401684cdbe5fb0c08719d7ed1d85f8e951bdea56
abhisince1994 <abhijeetsapkal2@gmail.com>
2024-08-21 14:24:43 +0530

    studentObjWithAddTwoNum successful submition

---
+++ studentObjWithAddTwoNum.js
@@ -0,0 +1,11 @@
+const student ={
+    name:'ram',
+    age:21,
+    addTowNum:()=>{
+        let a=4;
+        let b=5;
+        let c=a+b;
+        console.log('addition of two numbers',c);
+    }
+}
+student.addTowNum();
