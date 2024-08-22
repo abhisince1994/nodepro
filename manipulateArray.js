2f6c56cab6dabda367ac081e055f89fc264adeb4
abhisince1994 <87871120+abhisince1994@users.noreply.github.com>
2024-08-22 13:12:42 +0530

    Update manipulateArray.js

--- manipulateArray.js
+++ manipulateArray.js
@@ -1,10 +1,13 @@
-const person={
-    name: 'Max',
-    age: 29,
-    greet(){
-        console.log('hi,i am',this.name);
-    }
+const fruits=['apple','oranges','','mango','','lemon'];
+
+const indicesToInsert=[2,4];
+const stringToInsert='empty string';
+
+const transformedFruits = fruits.map((item,index)=>{
+if(indecesToInsert.include(index)){
+    return stringToInsert;
+}else{
+    return item;
 }
-const fruits=['apple','oranges','mangos','lemon'];
-fruits.splice(3,0,'empty String');
-console.log(fruits);
+});
+console.log(transformedFruits);
